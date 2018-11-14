c42be6d455fde40db4b9afc4cbd4b163e7ca2413
renjiawen <renjiawen@isoubu.com>
2018-11-14 13:39:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/sb_cm/rjwwidget/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.sb_cm.rjwwidget;
+
+import android.Manifest;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.sb_cm.rjwwidget.countdownview.CountDownTimerView;
+import com.example.sb_cm.rjwwidget.countdownview.OnTimerFinishListener;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        CountDownTimerView countDownTimerProcess = (CountDownTimerView) findViewById(R.id.countdown);
+
+        countDownTimerProcess.setTime(3, 4, 5);
+        countDownTimerProcess.start();
+        countDownTimerProcess.setOnFinishListener(new OnTimerFinishListener() {
+            @Override
+            public void finish() {
+                //结束
+                Toast.makeText(MainActivity.this, "倒计时结束了", Toast.LENGTH_LONG).show();
+
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/sb_cm/rjwwidget/countdownview/OnTimerFinishListener.java
@@ -0,0 +1,10 @@
+package com.example.sb_cm.rjwwidget.countdownview;
+
+/*
+ *  Des
+ *
+ * Created by renjiawen on 2018/10/23 下午1:40.
+ */
+public interface OnTimerFinishListener {
+    void finish();
+}
